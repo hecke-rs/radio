15390bd47a6742fd283de8037f860044a5a9b5f1
Erin Moon <erin@hashbang.sh>
2018-04-26 13:55:35 -0500

    deploy: simplify default.nix

--- default.nix
+++ default.nix
@@ -6,7 +6,7 @@ in
     frontend = stdenv.mkDerivation {
       name = "web-0.1";
       src = ./frontend;
-      buildInputs = [ pkgs.elmPackages.elm pkgs.elmPackages.elm-make ];
+      buildInputs = with pkgs.elmPackages; [ elm elm-make ];
       # default build phase will call make for us; we don't need to do that.
       installPhase = ''
         mkdir -p $out/
